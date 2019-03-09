1ff2a87847f646b21b56d7680bdb9bfd9e11c938
chengangcode <chengangxz@163.com>
2019-03-09 18:28:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <html>
     <head>
-        <title>demo</title>
+        <title>socket</title>
         <script src="https://cdn.bootcss.com/jquery/1.9.1/jquery.min.js"></script>
+    <style type="text/css">
+    .socket_center{
+        margin:0 auto;
+        width: 100px;
+        height: 100x;
+        background: green;
+    }
+    </style>
     </head>
     <body>
-    <input type="text" id="content">
-    <input type="button" value="send" id="send">
-    <div id="message_info"></div>
+        <br>
+        <br>
+        <div class="socket_center">
+            <input type="text" id="content">
+            <input type="button" value="send" id="send">
+            <div id="message_info"></div>
+        </div>
+    
         <script type="text/javascript">
             var ws = new WebSocket("ws://118.24.16.28:4000");
             ws.onopen = function(){
