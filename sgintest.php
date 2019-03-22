7f182c3d3c77c731740a4e8db5453bcaae43031b
chengangcode <chengangxz@163.com>
2019-03-23 00:33:12 +0800

    Create sgintest.php

---
+++ sgintest.php
@@ -0,0 +1,23 @@
+<?php
+
+function MakeSign($data_sgin)
+{
+	//签名步骤一：按字典序排序参数
+	ksort($data_sgin);
+	$string = "";
+	foreach ($data_sgin as $k => $v)
+	{
+		if($k != "sign"){
+			$buff .= $k . "=" . $v . "&";
+		}
+	}
+	$string = trim($string, "&");
+
+	//签名步骤二：在string后加入KEY
+	$string = $string . "&key=".$key;
+	//签名步骤三：MD5加密或者HMAC-SHA256
+	$string = hash_hmac("sha256",$string ,$config->GetKey());
+	//签名步骤四：所有字符转为大写
+	$result = strtoupper($string);
+	return $result;
+}
