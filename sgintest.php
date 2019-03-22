475f97160cb07809ab446e40716ea982bf317e3b
chengangcode <chengangxz@163.com>
2019-03-23 00:34:21 +0800

    Update sgintest.php

--- sgintest.php
+++ sgintest.php
@@ -1,6 +1,6 @@
 <?php
 
-function MakeSign($data_sgin)
+function MakeSign($data_sgin,$key)
 {
 	//签名步骤一：按字典序排序参数
 	ksort($data_sgin);
@@ -16,7 +16,7 @@ function MakeSign($data_sgin)
 	//签名步骤二：在string后加入KEY
 	$string = $string . "&key=".$key;
 	//签名步骤三：MD5加密或者HMAC-SHA256
-	$string = hash_hmac("sha256",$string ,$config->GetKey());
+	$string = hash_hmac("sha256",$string ,$key);
 	//签名步骤四：所有字符转为大写
 	$result = strtoupper($string);
 	return $result;
